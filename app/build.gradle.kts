b4e4ebe4011db1594e351584700575c541ded25c
ThomasGRG <60728981+ThomasGRG@users.noreply.github.com>
2024-03-14 16:49:20 +0530

    Release v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "jp.ikigai.kwallet"
         minSdk = 30
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
